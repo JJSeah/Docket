9f82abcad268b9a9dfe6a14c5723e2d8d91c1e78
Jeremy John <jjseah00@gmail.com>
2020-06-03 00:28:31 +0800

    Initial commit

---
+++ lib/model/hero_id_model.dart
@@ -0,0 +1,18 @@
+import 'package:meta/meta.dart';
+
+class HeroId {
+  final String progressId;
+  final String titleId;
+  final String codePointId;
+  final String remainingTaskId;
+  final String taskleft;
+
+
+  HeroId({
+    @required this.progressId,
+    @required this.titleId,
+    @required this.codePointId,
+    @required this.remainingTaskId,
+    @required this.taskleft,
+  });
+}
